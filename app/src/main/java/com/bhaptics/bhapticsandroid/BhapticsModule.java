a15a59e892e77b9bab12fefc8df5e18e22afb4fd
Sangwon Choi <sangwon.choi@bhaptics.com>
2020-06-19 15:37:54 +0900

    Fix device scanning on tegway

--- app/src/main/java/com/bhaptics/bhapticsandroid/BhapticsModule.java
+++ app/src/main/java/com/bhaptics/bhapticsandroid/BhapticsModule.java
@@ -12,11 +12,14 @@ import com.bhaptics.bhapticsmanger.BhapticsManager;
 import com.bhaptics.bhapticsmanger.BhapticsManagerImpl;
 import com.bhaptics.bhapticsmanger.HapticPlayer;
 import com.bhaptics.bhapticsmanger.HapticPlayerImpl;
+import com.bhaptics.ble.PairedDeviceManager;
+import com.bhaptics.ble.PairedDeviceManagerImpl;
 
 public class BhapticsModule {
     public static final String TAG = BhapticsModule.class.getSimpleName();
     private static BhapticsManager bhapticsManager;
     private static HapticPlayer hapticPlayer;
+    private static  PairedDeviceManager pairedDeviceManager;
 
     public static void initialize(Context context) {
         if (hapticPlayer != null) {
@@ -25,7 +28,8 @@ public class BhapticsModule {
 
 
         hapticPlayer = new HapticPlayerImpl();
-        bhapticsManager = new BhapticsManagerImpl(context, hapticPlayer);
+        PairedDeviceManager pairedDeviceManager = new PairedDeviceManagerImpl(context);
+        bhapticsManager = new BhapticsManagerImpl(context, hapticPlayer, pairedDeviceManager);
     }
 
     public static void destroy() {
